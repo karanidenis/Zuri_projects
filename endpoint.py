e391d80fe8b6a805eb9b9c1b4d46a4257ebe0cc3
karanidenis <d.waweru@alustudent.com>
2023-09-12 17:04:50 +0300

    new changes

--- endpoint.py
+++ endpoint.py
@@ -2,6 +2,7 @@ import datetime
 import pytz
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 
 app = Flask(__name__)
@@ -68,4 +69,5 @@ def endpoint():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
