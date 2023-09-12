d7fdd40b63a66c139e5128d678669114b2712273
karanidenis <d.waweru@alustudent.com>
2023-09-12 10:18:01 +0300

    endpoint returning a JSON format

---
+++ README.md
@@ -0,0 +1,25 @@
+## Usage
+To use this API, fork the repository and clone it to your local machine. Then, clone the repository to your local machine:
+```bash
+git clone https://github.com/karanidenis/Zuri_projects/tree/main
+```
+After cloning, cd into the directory:
+```bash
+cd Zuri_projects
+```
+Then, run the following command to install the required dependencies:
+```bash
+pip install -r requirements.txt
+```
+After installing the dependencies, run the following command in your terminal:
+```bash
+python3 endpoint.py
+```
+Then, navigate to `http://localhost:5000/endpoint?slack_name=exampple&track=Frontend` on your browser to access the API.
+
+## Author
+- [Karani Denis](https://github.com/karanidenis/Zuri_projects/tree/main) - Full Stack Developer (Python, Flask, React, SQL, Docker, CI/CD e.t.c) 
+
+## Acknowledgments
+- [Zuri Team](https://internship.zuri.team/) - For the opportunity to work on this project.
+- [Ingressive4Good](https://ingressive.org/) - For the opportunity to work on this project.
--- endpoint.py
+++ endpoint.py
@@ -1,94 +1,71 @@
-# Create and host an endpoint using any programming language of your choice.
-# The endpoint should take two GET request query parameters and return specific information in JSON format.Requirements
-# The information required includes:
-
-#     Slack name
-#     Current day of the week
-#     Current UTC time (with validation of +/-2)
-#     Track
-#     The GitHub URL of the file being run
-#     The GitHub URL of the full source code.
-#     A  Status Code of Success
-
-# JSON
-
-#     {
-#       "slack_name": "example_name",
-#       "current_day": "Monday",
-#       "utc_time": "2023-08-21T15:04:05Z",
-#       "track": "backend",
-#       "github_file_url": "https://github.com/username/repo/blob/main/file_name.ext",
-#       "github_repo_url": "https://github.com/username/repo",
-#       “status_code”: 200
-#     }
-
-
-import requests
-import json
 import datetime
-import time
-import os
-import sys
-import re
 import pytz
 from flask import Flask, request, jsonify
-from flask_restful import Resource, Api
 from flask_cors import CORS
 
 
 app = Flask(__name__)
-api = Api(app)
 CORS(app)
 
-# Get the current day of the week
+
 def get_current_day():
+    # Get the current day of the week
     day = datetime.datetime.now().strftime("%A")
     return day
 
-# Get the current UTC time
-def get_current_utc_time():
-    utc_time = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
-    return utc_time
 
-# Get the current UTC time with timezone    
 def get_current_utc_time_with_timezone():
-    utc_time = datetime.datetime.now(pytz.timezone('UTC')).strftime("%Y-%m-%dT%H:%M:%SZ")
+    # Get the current UTC time with timezone
+    utc_time = datetime.datetime.now(
+        pytz.timezone('UTC')).strftime("%Y-%m-%dT%H:%M:%SZ")
     return utc_time
 
-# Track
-def get_track():
-    track = "backend"
-    return track
 
-# Get the GitHub URL of the file being run
 def get_github_file_url():
-    github_file_url = ""
+    # Get the GitHub URL of the file being run
+    github_file_url = "https://github.com/karanidenis/Zuri_projects/blob/main/endpoint.py"
     return github_file_url
 
-# Get the GitHub URL of the full source code
+
 def get_github_repo_url():
-    github_repo_url = ""
+    # Get the GitHub URL of the full source code repo
+    github_repo_url = "https://github.com/karanidenis/Zuri_projects/tree/main"
     return github_repo_url
-    
-# Get the status code
-def get_status_code():
-    status_code = 200
-    return status_code
 
-# Get the slack name
-def get_slack_name():
-    slack_name = ""
-    return slack_name
-    
 
-# Endpoint
 @app.route('/endpoint', methods=['GET'])
+# Endpoint
 def endpoint():
-    slack_name = get_slack_name()
+    # Get the "slack_name" and "track" query parameters from the request
+    slack_name = request.args.get("slack_name")
+    track = request.args.get("track")
+
+    if slack_name is None or track is None:
+        # If any of the parameters are missing, return a 400 Bad Request status
+        return jsonify({"message": "Bad Request - Missing Parameters"}), 400
+
     current_day = get_current_day()
     utc_time = get_current_utc_time_with_timezone()
-    track = get_track()
     github_file_url = get_github_file_url()
     github_repo_url = get_github_repo_url()
-    status_code = get_status_code()
-    return jsonify(slack_name=slack_name, current_day=current_day, utc_time=utc_time, track=track, github_file_url=github_file_url, github_repo_url=github_repo_url, status_code=status_code)
+    status_code = 200
+    response_data = {
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": utc_time,
+        "track": track,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": status_code
+    }
+    try:
+        return jsonify(response_data)
+
+    except Exception as e:
+        status_code = 400
+        print(jsonify({"message": "Error"}), 400)
+        return jsonify(response_data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
